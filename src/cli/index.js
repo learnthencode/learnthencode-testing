b2fe7b58f322f0af379cd42a30d43102a88c40dd
learnthencode <302176100+learnthencode@users.noreply.github.com>
2026-07-14 12:53:30 +0300

    Add CLI message constants

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,11 +1,9 @@
+import { MESSAGES } from "../constants/messages.js";
+
 /**
  * Starts the LearnThenCode Testing Framework CLI.
  */
 export function start() {
-  console.log("========================================");
-  console.log(" LearnThenCode Testing Framework");
-  console.log(" Version 1.0.0");
-  console.log("========================================");
-  console.log();
-  console.log("Initializing...");
+  console.log(MESSAGES.banner);
+  console.log(MESSAGES.welcome);
 }
---
+++ src/constants/messages.js
@@ -0,0 +1,33 @@
+export const MESSAGES = {
+  banner: `
+========================================
+ LearnThenCode Testing Framework
+ Version 1.0.0
+========================================
+`,
+
+  welcome:
+    "Welcome to the LearnThenCode Testing Framework.",
+
+  help:
+    `
+LearnThenCode Testing Framework
+
+Usage:
+  learnthencode-test <command>
+
+Commands:
+  run        Run learner tests
+  help       Display help information
+
+Options:
+  --version  Display version information
+  --help     Display help information
+`,
+
+  version:
+    "LearnThenCode Testing Framework v1.0.0",
+
+  unknownCommand:
+    "Unknown command. Use --help to see available commands."
+};
